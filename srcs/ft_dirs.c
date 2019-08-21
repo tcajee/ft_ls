3fe03e7ef8c2459784a473123929743624ccd2a4
Tameem CAJEE <tcajee@c5r3s1.wethinkcode.co.za>
2019-08-21 11:39:40 +0200

    fixed initial name print bug on -R and cleaned repo

--- srcs/ft_dirs.c
+++ srcs/ft_dirs.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 16:23:43 by tcajee            #+#    #+#             */
-/*   Updated: 2019/08/20 18:37:42 by sminnaar         ###   ########.fr       */
+/*   Updated: 2019/08/21 11:31:04 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,8 +85,8 @@ int ft_dirs(t_flags *flags, t_info dirs[], char *path)
 
 	F(!path, ft_dir_info(".", dirs));
 	_F_(!ft_dir_info(path, dirs), ft_errors(E_DIRS, path));
-	F((*flags & F_M || *flags & F_R) && *flags & F_P, ft_print_f("n"));
-	F(*flags & F_M || *flags & F_R, ft_print_f("%:n", dirs[0].root));
+	F((*flags & F_M || *flags & F_R) && *flags & F_P,
+	F(!!(ft_print_f("n")), ft_print_f("%:n", dirs[0].root)));
 	F(*flags & F_l, ft_print_f("%%n", "total: ", dirs[0].total));
 	ft_prints(flags, dirs);
 	ft_dir_clean(dirs);
