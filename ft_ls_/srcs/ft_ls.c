de301e044330008584a2aee499a9528ddf3e0355
Tameem CAJEE <tcajee@c5r3s4.wethinkcode.co.za>
2019-08-23 14:28:21 +0200

    testing

--- ft_ls_/srcs/ft_ls.c
+++ ft_ls_/srcs/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/29 13:13:54 by tcajee            #+#    #+#             */
-/*   Updated: 2019/08/23 13:52:26 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/08/23 14:28:13 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_flags		flags;
 	t_info		dirs[32768];
 	int			i;
-	int			j;
+	/* int			j; */
 
 	i = 0;
 	if (!(flags = 0))
@@ -31,19 +31,22 @@ int	main(int argc, char **argv)
 	/* ft_putendl(""); */
 	/* printf("%s\n", argv[i]); */
 
-	j = i - 1;
-	if (!argv[i])
-		return (ft_dirs(&flags, dirs, "."));
-	while (argv[++j])
-	{
-		if (!(ft_dir_check(argv[j])))
-			ft_errors(E_PRINTS, argv[j]);
-	}
-	j = i - 1;
-	while (argv[++j])
-	{
-		if (ft_dir_check(argv[j]))
-			ft_dirs(&flags, dirs, argv[j]);
-	}
+	/* j = i - 1; */
+	/* if (!argv[i]) */
+	/* 	return (ft_dirs(&flags, dirs, ".")); */
+	/* while (argv[++j]) */
+	/* { */
+	/* 	if (!(ft_dir_check(argv[j]))) */
+	/* 		ft_errors(E_PRINTS, argv[j]); */
+	/* } */
+	/* j = i - 1; */
+	/* while (argv[++j]) */
+	/* { */
+	/* 	if (ft_dir_check(argv[j])) */
+	/* 		ft_dirs(&flags, dirs, argv[j]); */
+	/* } */
+	
+	ft_dirs(&flags, dirs, argv[1]);
+
 	return (0);
 }
