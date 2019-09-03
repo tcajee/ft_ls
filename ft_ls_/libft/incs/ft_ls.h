44d77abda8c88381bb4df89bfefe52cb899cf5b9
Tameem CAJEE <tcajee@c5r3s4.wethinkcode.co.za>
2019-09-03 17:15:59 +0200

    ALL FILES NORMED

--- ft_ls_/libft/incs/ft_ls.h
+++ ft_ls_/libft/incs/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 14:36:21 by tcajee            #+#    #+#             */
-/*   Updated: 2019/09/03 17:07:09 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/09/03 17:15:49 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,40 +39,31 @@
 # define F_0 0
 
 # define F_1 1
-# define F_l 2
-# define F_g 4
-# define F_t 8
-# define F_u 16
-# define F_r 32
-# define F_f 64
-# define F_a 128
+# define F_L 2
+# define F_G 4
+# define F_T 8
+# define F_U 16
+# define F_R 32
+# define F_F 64
+# define F_A 128
 
-# define F_R 256
-# define F_d 512
-# define F_F 1024
-# define F_G 2048
-# define F_L 4096
-# define F_P 8192
-# define F_00 16384
-# define F_X 32768
+# define F_RR 256
+# define F_D 512
+# define F_FF 1024
+# define F_GG 2048
+# define F_LL 4096
+# define F_PP 8192
+# define F_2 16384
+# define F_3 32768
 
 # define F_I 65536
 # define F_M 131072
 # define F_REG 262144
-# define F_5 524288
-# define F_6 1048576
-# define F_7 2097152
-# define F_8 4194304
-# define F_9 8388608
-
-/* # define F_I 65536 */
-/* # define F_M 131072 */
-/* # define F_4 262144 */
-/* # define F_5 524288 */
-/* # define F_6 1048576 */
-/* # define F_7 2097152 */
-/* # define F_8 4194304 */
-/* # define F_9 8388608 */
+# define F_4 524288
+# define F_5 1048576
+# define F_6 2097152
+# define F_7 4194304
+# define F_8 8388608
 
 typedef struct stat		t_stat;
 typedef struct dirent	t_dirent;
@@ -125,9 +116,9 @@ int						ft_dir_fill(int *flags, t_dirs *dirs, char *path);
 void					ft_dir_form(int *flags, t_dirs *dirs);
 
 void					ft_sorts(int *flags, t_dirs *dirs);
-void					ft_sort_lex(int *flags, t_info **sorted, t_info *unsorted);
-void					ft_sort_time(int *flags, t_info **sorted, t_info *unsorted);
-t_info					*ft_sort_comp(int *flags, t_info *list, t_info* unsorted);
+void					ft_sort_lex(int *flags, t_info **sort, t_info *unsort);
+void					ft_sort_time(int *flags, t_info **sort, t_info *unsort);
+t_info					*ft_sort_comp(int *flags, t_info *list, t_info *unsort);
 void					ft_sort_clean(t_dirs *dirs);
 
 int						ft_prints(int *flags, t_dirs *dirs);
