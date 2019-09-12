edbaed8add5f331d4f26a8c9e99db7de2f948f90
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-09-12 10:09:55 +0200

    Sorted out ft_sorts

--- ft_ls_/srcs/ft_sorts.c
+++ ft_ls_/srcs/ft_sorts.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/02 14:16:47 by tcajee            #+#    #+#             */
-/*   Updated: 2019/09/11 15:00:50 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/09/12 10:08:21 by sminnaar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,9 +53,9 @@ int		ft_sort_comp(int *flags, t_info *a, t_info *b)
 	{
 		if (a->s_stat.st_atimespec.tv_sec == b->s_stat.st_atimespec.tv_sec)
 		{
-			return (a->s_stat.st_mtime != b->s_stat.st_mtimespec.tv_nsec) ?
-			(a->s_stat.st_mtimespec.tv_nsec < b->s_stat.st_mtimespec.tv_nsec) :
-			(ft_strcmp(a->name, b->name));
+			return (a->s_stat.st_atimespec.tv_nsec !=
+			b->s_stat.st_atimespec.tv_nsec) ? (a->s_stat.st_atimespec.tv_nsec <
+			b->s_stat.st_atimespec.tv_nsec) : (ft_strcmp(a->name, b->name));
 		}
 		else
 			return (a->s_stat.st_atime < b->s_stat.st_atime);
