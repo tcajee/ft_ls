094e671f1980f4372e2040843fddfd3933b882b1
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-11-25 10:11:19 +0200

    Updating

--- ft_ls.sh
+++ ft_ls.sh
@@ -4,69 +4,69 @@ echo "Testing ft_ls submission by: tcajee"
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls"
-cd ~/Desktop/smsibi && ./ft_ls
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -a"
-cd ~/Desktop/smsibi && ./ft_ls -a
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -a
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -l"
-cd ~/Desktop/smsibi && ./ft_ls -l
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -l
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -l /"
-cd ~/Desktop/smsibi && ./ft_ls -l /
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -l /
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -r"
-cd ~/Desktop/smsibi && ./ft_ls -r
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -r
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -t"
-cd ~/Desktop/smsibi && ./ft_ls -lt
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -lt
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -Rl"
-cd ~/Desktop/smsibi && ./ft_ls -Rl
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -Rl
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -r author build Makefile srcs"
-cd ~/Desktop/smsibi && ./ft_ls -r author build Makefile srcs
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -r author build Makefile srcs
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -l author build Makefile srcs"
-cd ~/Desktop/smsibi && ./ft_ls -l author build Makefile srcs
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -l author build Makefile srcs
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -l -t -a -r"
-cd ~/Desktop/smsibi && ./ft_ls -l -t -a
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -l -t -a
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -ltar"
-cd ~/Desktop/smsibi && ./ft_ls -ltar
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -ltar
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls invalid"
-cd ~/Desktop/smsibi && ./ft_ls invalid
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls invalid
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls with inaccesible folder"
 echo "Making temp folder"
-cd ~/Desktop/smsibi && mkdir temp
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && mkdir temp
 echo "Setting permissions"
-cd ~/Desktop/smsibi && chmod 000 temp
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && chmod 000 temp
 echo "Testing ./ft_ls temp"
-cd ~/Desktop/smsibi && ./ft_ls temp
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls temp
 read -p "Press enter to continue"
 echo "Re-setting permissions"
-cd ~/Desktop/smsibi && chmod 777 temp
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && chmod 777 temp
 echo "Cleaning"
-cd ~/Desktop/smsibi && rm -rf temp
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && rm -rf temp
 read -p "Press enter to continue"
 clear
 echo "Testing ./ft_ls -laXr"
-cd ~/Desktop/smsibi && ./ft_ls -laXr
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ./ft_ls -laXr
 read -p "Press enter to continue"
 clear
 echo "Testing Complete"
--- ls.sh
+++ ls.sh
@@ -5,60 +5,60 @@ echo "Testing ft_ls submission by: tcajee"
 read -p "Press enter to continue"
 clear
 echo "Testing ls -1"
-cd ~/Desktop/smsibi && ls -1
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -1
 read -p "Press enter to continue"
 clear
 echo "Testing ls -1a"
-cd ~/Desktop/smsibi && ls -1a
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -1a
 read -p "Press enter to continue"
 clear
 echo "Testing ls -l"
-cd ~/Desktop/smsibi && ls -l
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -l
 read -p "Press enter to continue"
 clear
 echo "Testing ls -l /"
-cd ~/Desktop/smsibi && ls -l /
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -l /
 read -p "Press enter to continue"
 clear
 echo "Testing ls -1r"
-cd ~/Desktop/smsibi && ls -1r
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -1r
 read -p "Press enter to continue"
 clear
 echo "Testing ls -lt"
-cd ~/Desktop/smsibi && ls -lt
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -lt
 read -p "Press enter to continue"
 clear
 echo "Testing ls -lR"
-cd ~/Desktop/smsibi && ls -lR
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -lR
 read -p "Press enter to continue"
 clear
 echo "Testing ls -1r author build Makefile srcs"
-cd ~/Desktop/smsibi && ls -1r author build Makefile srcs
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -1r author build Makefile srcs
 read -p "Press enter to continue"
 clear
 echo "Testing ls -l author build Makefile srcs"
-cd ~/Desktop/smsibi && ls -l author build Makefile srcs
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -l author build Makefile srcs
 read -p "Press enter to continue"
 clear
 echo "Testing ls -l -t -a -r"
-cd ~/Desktop/smsibi && ls -l -t -a
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -l -t -a
 read -p "Press enter to continue"
 clear
 echo "Testing ls -ltar"
-cd ~/Desktop/smsibi && ls -ltar
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -ltar
 read -p "Press enter to continue"
 clear
 echo "Testing ls -1 invalid"
-cd ~/Desktop/smsibi && ls -1 invalid
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -1 invalid
 read -p "Press enter to continue"
 clear
 echo "Testing ls with inaccesible folder"
 echo "Testing ls -1 temp"
-cd ~/Desktop/smsibi && ls -1 temp
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -1 temp
 read -p "Press enter to continue"
 clear
 echo "Testing ls -laXr"
-cd ~/Desktop/smsibi && ls -laXr
+cd /home/emptycee/Desktop/repos/ft_ls_git/ft_ls_final && ls -laXr
 read -p "Press enter to continue"
 clear
 echo "Testing Complete"
